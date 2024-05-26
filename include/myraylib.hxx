ff0bff472b73fe8b64cc71696baeea982367a0c3
Mu Lu <murphytalk@gmail.com>
2024-05-26 19:18:55 +0900

    TAB => space

--- include/myraylib.hxx
+++ include/myraylib.hxx
@@ -7,27 +7,27 @@
 
 template <class App, typename... Args>
 void myraylib(int width, int height, const char* title, const int fps,
-	// App constructor parameters
-	Args &&... args
+    // App constructor parameters
+    Args &&... args
 ) {
-	raylib::Window window(width, height, title);
+    raylib::Window window(width, height, title);
 
-	auto app = std::make_unique<App>(std::forward<Args>(args)...);
+    auto app = std::make_unique<App>(std::forward<Args>(args)...);
 
-	SetTargetFPS(fps);
-	while (!window.ShouldClose()) {    // Detect window close button or ESC key
-		// Update
-		//----------------------------------------------------------------------------------
-		// Update your variables here
-		//----------------------------------------------------------------------------------
-		app->update(window, width, height);
+    SetTargetFPS(fps);
+    while (!window.ShouldClose()) {    // Detect window close button or ESC key
+        // Update
+        //----------------------------------------------------------------------------------
+        // Update your variables here
+        //----------------------------------------------------------------------------------
+        app->update(window, width, height);
 
-		// Draw
-		//----------------------------------------------------------------------------------
-		BeginDrawing();
-			app->draw(window, width, height);
-		EndDrawing();
-		//----------------------------------------------------------------------------------
-	}
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+        app->draw(window, width, height);
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
 }
 
